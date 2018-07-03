80cfcbb7df0057bd7b8a5e60980a86c2c018270f
Luke Hoban <luke@pulumi.com>
2018-07-03 09:02:57 -0700

    Clean up example

--- examples/blob/index.ts
+++ examples/blob/index.ts
@@ -4,30 +4,10 @@ import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure";
 import * as serverless from "@pulumi/azure-serverless";
 
-const subscriptionId = pulumi.output(azure.core.getSubscription({}).then(s => s.subscriptionId));
-const config = new pulumi.Config("azure-blob");
-const clientId = config.require("clientId");
-const clientSecret = config.require("clientSecret");
-const tenant = config.require("tenant");
 
 const resourceGroup = new azure.core.ResourceGroup("resourcegroup", {
     location: "West US 2",
 });
-
-const nginx = new azure.containerservice.Group("containergroup", {
-    location: resourceGroup.location,
-    resourceGroupName: resourceGroup.name,
-    ipAddressType: "public",
-    osType: "linux",
-    containers: [{
-        name: "nginx",
-        image: "nginx",
-        cpu: 0.5,
-        memory: 1.5,
-        port: 80
-    }],
-});
-
 // Create a storage account for our images
 const storageAccount = new azure.storage.Account("storage", {
     resourceGroupName: resourceGroup.name,
@@ -45,16 +25,10 @@ const storageContainer = new azure.storage.Container("images-container", {
 
 // When a new PNG image is added, fire an event
 serverless.blob.onEvent("newImage", storageAccount, "images/{name}.png", (context, blob) => {
-    (async () => {
-        context.log!(context);
-        context.log!(blob);
-
-        const {default: fetch} = await import("node-fetch");
-        const res = await fetch(`http://${nginx.ipAddress.get()}`);
-        const text = await res.text()
-        context.log!(text);
-    })().then(_ => context.done(), context.done);
+    context.log!(context);
+    context.log!(blob);
+    context.done();
 }, {resourceGroup: resourceGroup});
 
-// Export the IP address of the container
-export let ipAddress = nginx.ipAddress;
+// The storage account where images should be uploaded
+export let storageAccountName = storageAccount.name;
